8dcc110381a7583331099165e209ffebfc422e71
Aumizy <aumizy01@gmail.com>
2024-08-18 23:27:28 -0700

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,7 @@
 # Increase number of requests handled by Nginx
 
+
+
 exec { 'set_ulimit_to 5000':
   command => '/bin/sed -i "s/ULIMIT.*/ULIMIT=\"-n 5000\"/" /etc/default/nginx'
 
--- 0x1B-web_stack_debugging_4/README.md
+++ 0x1B-web_stack_debugging_4/README.md
@@ -1,1 +1,1 @@
-THIS IS A README FILE FOR 0x1B-web_stack_debugging_4
+THIS IS A README FILE FOR 0x1B-web_stack_debugging_4 project
